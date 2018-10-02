1fec0275ebbd768ea578d3a658111cf0d6dcf35a
utleroy <bryanhankins@me.com>
2018-10-02 17:37:22 -0500

    add routing and links for products list and create components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ProductListComponent } from './product-list/product-list.component';
+import { ProductCreateComponent } from './product-create/product-create.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'product-list', pathMatch: 'full' },
+  {
+    path: 'product-list',
+    component: ProductListComponent
+  },
+  {
+    path: 'product-create',
+    component: ProductCreateComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
